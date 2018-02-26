d15e2dfe22373cfcde434d18ed90d78edd317009
caiwb <987220036@qq.com>
2018-02-26 10:29:12 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/example/caiwb/weathertest/WeatherActivity.java
+++ app/src/main/java/com/example/caiwb/weathertest/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.caiwb.weathertest;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.caiwb.weathertest.gson.Forecast;
 import com.example.caiwb.weathertest.gson.Weather;
+import com.example.caiwb.weathertest.service.AutoUpdateService;
 import com.example.caiwb.weathertest.util.HttpUtil;
 import com.example.caiwb.weathertest.util.Utility;
 
@@ -224,5 +226,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
